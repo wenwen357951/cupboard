cdc871dafd267ede7575284fcf8958102e606d8e
Mics <mics@mics.tw>
2016-04-10 02:17:50 +0800

    better right click, add snick right click has infomation

--- src/com/mics/spigotPlugin/cupboard/Cupboard.java
+++ src/com/mics/spigotPlugin/cupboard/Cupboard.java
@@ -39,44 +39,58 @@ public class Cupboard extends JavaPlugin implements Listener {
     @EventHandler
     public void onGoldBlockBreak(BlockBreakEvent event){
     	if(event.getBlock().getType() == Material.GOLD_BLOCK){
-    		//Player p = event.getPlayer();
+    		Player p = event.getPlayer();
     		data.removeCupboard(event.getBlock());
-    		//Util.msgToPlayer(p, "已移除工具櫃");
+    		Util.msgToPlayer(p, "金磚已拆除");
     	}
     }
     
     //放置金磚
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onGoldBlockPlace(BlockPlaceEvent event){
     	if(event.getBlockPlaced().getType() == Material.GOLD_BLOCK){
     		Player p = event.getPlayer();
     		if(!data.putCupboard(event.getBlockPlaced(), p)){
-    			Util.msgToPlayer(p, "距離其他工具櫃太近。");
+    			Util.msgToPlayer(p, "距離其他金磚太近。");
     			event.setCancelled(true);
     			return;
     		}
-    		Util.msgToPlayer(p, "工具櫃已經放置並取得授權。");
+    		Util.msgToPlayer(p, "金磚已放置並取得授權。(蹲下右鍵取得說明)");
     	}
     }
 
     
     //授權/取消授權
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onRightClick(PlayerInteractEvent event){
         if (event.getHand() == EquipmentSlot.OFF_HAND) return;                    		// off hand packet, ignore.
-    	if (event.getClickedBlock().getType() != Material.GOLD_BLOCK) return;       	// 非黃金磚則無視
+    	if (event.getClickedBlock() == null || event.getClickedBlock().getType() != Material.GOLD_BLOCK) return;       	// 非黃金磚則無視
     	if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return; 						// 非右鍵方塊則無視
-        if (event.getItem() != null && event.getItem().getType().isBlock()) return;   	// 非空手則無視
-    	
+        //if (event.getItem() != null && event.getItem().getType().isBlock()) return;   	// 非空手則無視
 		Player p = event.getPlayer();
+		if (p.isSneaking()){
+			//說明
+			p.sendMessage("§a金磚放置後產生19x19x19的方形保護區，金磚在正中央。");
+			p.sendMessage("§a保護區只防止下列事項: ");
+			p.sendMessage("§71. 未授權者無法放置/移除方塊。");
+			p.sendMessage("§72. 未授權者無法使用石製壓力版/石製按鈕。");
+			p.sendMessage("§73. 防止Creeper/TNT炸掉保護區。");
+			p.sendMessage("§73. 防止黃金磚被活塞推動。");
+			p.sendMessage("§c特別警告以下為可執行事件: ");
+			p.sendMessage("§71. 怪物仍可觸發石製壓力版");
+			p.sendMessage("§72. 使用木製門/木製按鈕/木製踏板/控制桿/活塞");
+			p.sendMessage("§73. 打掉下面方塊使會掉落之方塊掉落");
+			return;
+		}
 		String str;
 		if(!data.checkCupboardExist(event.getClickedBlock())){
-			str="此方塊並非由玩家放置或資料遺失，請拆除後重新放置";
+			str="此金磚非由玩家放置或資料遺失，請拆除後重新放置";
 		}else if(data.toggleBoardAccess(p, event.getClickedBlock())){
-			str="工具櫃已授權";
+			str="已授權";
 		} else {
-			str="工具櫃已取消授權";
+			str="已取消授權";
 		}
+		event.setCancelled(true);
 		Util.msgToPlayer(p, str);
     }
     
@@ -125,7 +139,7 @@ public class Cupboard extends JavaPlugin implements Listener {
     }
     
     //防止玩家使用水桶
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onBucketEmpty(PlayerBucketEmptyEvent e){
     	Player p = e.getPlayer();
     	Block b = e.getBlockClicked().getLocation()
@@ -138,7 +152,7 @@ public class Cupboard extends JavaPlugin implements Listener {
         	}
         }
     }
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onBucketFill(PlayerBucketFillEvent e){
     	Player p = e.getPlayer();
     	Block b = e.getBlockClicked();
@@ -151,7 +165,7 @@ public class Cupboard extends JavaPlugin implements Listener {
     }
     
     //TNT or Creeper爆炸
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onExplode(EntityExplodeEvent event){
     	for (Block block : new ArrayList<Block>(event.blockList())){
     		if(data.checkIsLimit(block)){
@@ -162,7 +176,7 @@ public class Cupboard extends JavaPlugin implements Listener {
     
     //防止方塊被燒壞
     // TODO 火焰不會消失
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH)
     void onBlockBurnDamage(BlockBurnEvent e){
     	Block b = e.getBlock();
     	if(data.checkIsLimit(b))
