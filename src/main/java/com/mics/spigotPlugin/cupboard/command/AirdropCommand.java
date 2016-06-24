9263090c7ea7927833d22b7f93d306235561bf75
Mics <mics@mics.tw>
2016-06-24 12:02:30 +0800

    add airdrop command 2

--- src/main/java/com/mics/spigotPlugin/cupboard/command/AirdropCommand.java
+++ src/main/java/com/mics/spigotPlugin/cupboard/command/AirdropCommand.java
@@ -7,30 +7,26 @@ import org.bukkit.entity.Player;
 
 import com.mics.spigotPlugin.cupboard.Cupboard;
 
-public class AirdropCommand {
-	public class ReloadCommand implements CommandExecutor{
-		Cupboard plugin;
-		public ReloadCommand(Cupboard i){
-			this.plugin = i;
-		}
-		@Override
-		public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
-			if (!(sender instanceof Player)) {
-				if( arg3.length == 3 ){
-					sender.sendMessage("Airdrop");
-				} else {
-					return false;
-				}
-			} else {
-				if( arg3.length == 0 ){
-					sender.sendMessage("Airdrop at sender loction");
-				} else if ( arg3.length == 0 ){
-					sender.sendMessage("Airdrop at static location");
-				} else {
-					return false;
-				}
+public class AirdropCommand  implements CommandExecutor {
+	Cupboard plugin;
+	public AirdropCommand(Cupboard i){
+		this.plugin = i;
+	}
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
+		if (arg3.length == 2 ){
+			try {
+				double x = Integer.valueOf(arg3[0]);
+				double z = Integer.valueOf(arg3[1]);
+				airdrop(x, z);
+				return true;
+			} catch (NumberFormatException err) {
 			}
-			return true;
+		} else if( (sender instanceof Player) && arg3.length == 0 ){
+			airdrop(((Player)sender).getLocation().getBlockX(), ((Player)sender).getLocation().getBlockZ());
 		}
+		return false;
+	}
+	private void airdrop(double x, double z){
+		//plugin.logDebug(String.format("Command Call Airdrop at %f %f location", x, z));
 	}
 }
