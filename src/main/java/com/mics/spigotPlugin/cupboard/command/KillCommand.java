ac7ef391cf5e45ec90121c3526437ca8a58831e2
Mics <mics@mics.tw>
2016-06-24 11:14:01 +0800

    1.2.5 remove suicide message
    移除自殺訊息

--- src/main/java/com/mics/spigotPlugin/cupboard/command/KillCommand.java
+++ src/main/java/com/mics/spigotPlugin/cupboard/command/KillCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+import org.bukkit.metadata.FixedMetadataValue;
 import com.mics.spigotPlugin.cupboard.Cupboard;
 
 
@@ -24,6 +24,7 @@ public class KillCommand implements CommandExecutor{
 			return true;
 		}
 		Player p = (Player) sender;
+		p.setMetadata("suicide", new FixedMetadataValue(plugin, "123"));
 		p.setHealth(0);
 		return true;
 	}
