ae5054a7f9fd2c807e725ee50a7b2a28250f9161
Mics <mics@mics.tw>
2016-11-24 23:47:34 +0800

    remove reloader and code clean

--- src/main/java/tw/mics/spigot/plugin/cupboard/Cupboard.java
+++ src/main/java/tw/mics/spigot/plugin/cupboard/Cupboard.java
@@ -9,7 +9,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import tw.mics.spigot.plugin.cupboard.command.GoldCommand;
-import tw.mics.spigot.plugin.cupboard.command.ReloadCommand;
 import tw.mics.spigot.plugin.cupboard.config.Config;
 import tw.mics.spigot.plugin.cupboard.config.Locales;
 import tw.mics.spigot.plugin.cupboard.data.CupboardsData;
@@ -39,21 +38,22 @@ public class Cupboard extends JavaPlugin implements Listener {
         Locales.load();
         this.logDebug("Loaded Locales!");
         
-        //load Airdrops
-        
         //load cupboards
         database = new Database(this, getDataFolder());
         cupboards = new CupboardsData(this, database.getConnection());
-        this.logDebug("Loaded Cupboards data!");
-
-        this.logDebug("Cleaning Cupboards data!");
         cupboards.cleanNotExistUser();
 
-        this.getCommand("cupboardreload").setExecutor(new ReloadCommand(this));
         this.getCommand("gold").setExecutor(new GoldCommand(this));
         
         registerObject();
     }
+    
+    @Override
+    public void onDisable() {
+        unregisterObject();
+        database.close();
+    }
+    
 	private void registerObject(){
 		//register listener
         registedObject.add(new CupboardEntityProtectListener(this));
@@ -67,62 +67,20 @@ public class Cupboard extends JavaPlugin implements Listener {
         	registedObject.add(new TNTCraftListener(this));
         }
 	}
-	private void unload(){
-	    this.logDebug("");
-        this.logDebug("============================================");
-        this.logDebug("Disconnect Database");
-        this.logDebug("============================================");
-        this.logDebug("");
-        database.close();
-        
-        this.logDebug("============================================");
-        this.logDebug("Unregister Object");
-        this.logDebug("============================================");
+	
+	private void unregisterObject(){
         for(Object l : registedObject){
             if(l instanceof MyListener){
                 ((MyListener)l).unregisterListener();
             } else {
-                this.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 this.log("[ERROR] Object " + l.getClass().getName() + " Can't unreigster");
-                this.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             }
         }
-
-        this.logDebug("");
-        this.logDebug("============================================");
-        this.logDebug("Unregister ALL Listener and Schedule tasks");
-        this.logDebug("============================================");
+        
         //force unregister again
-        this.logDebug("Unregister Listener!");
         HandlerList.unregisterAll();
-        this.logDebug("Unregister Schedule tasks!");
         this.getServer().getScheduler().cancelAllTasks();
 	}
-	public void reload(){
-	    unload();
-		//new registerdListeners
-		registedObject = new ArrayList<Object>();
-        //load config
-		this.logDebug("");
-		this.logDebug("============================================");
-		this.logDebug("Reloading Config / Locales");
-		this.logDebug("============================================");
-        Config.load();
-        this.logDebug("Loaded Config!");
-        Locales.load();
-        this.logDebug("Loaded Locales!");
-
-		this.logDebug("");
-		this.logDebug("============================================");
-		this.logDebug("Register Listener");
-		this.logDebug("============================================");
-		registerObject();
-		this.logDebug("");
-	}
-    @Override
-    public void onDisable() {
-    	unload(); //unload everything
-    }
     
     public boolean isOP(Player p){
     	return isOP(p, true);
