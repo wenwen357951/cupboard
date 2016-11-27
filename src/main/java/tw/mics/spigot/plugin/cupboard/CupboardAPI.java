298c7dda7827b29b2bbb5e4fe5573591f610d5b8
Mics <mics@mics.tw>
2016-11-27 10:33:16 +0800

    add new API

--- src/main/java/tw/mics/spigot/plugin/cupboard/CupboardAPI.java
+++ src/main/java/tw/mics/spigot/plugin/cupboard/CupboardAPI.java
@@ -1,10 +1,17 @@
 package tw.mics.spigot.plugin.cupboard;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class CupboardAPI {
     public static boolean checkIsLimit(Location l, Player p){
         return Cupboard.getInstance().cupboards.checkIsLimit(l, p);
+    }
+    public static boolean checkIsLimit(Location l){
+        return Cupboard.getInstance().cupboards.checkIsLimit(l);
+    }
+    public static boolean checkIsLimit(Block b){
+        return Cupboard.getInstance().cupboards.checkIsLimit(b);
     }
 }
